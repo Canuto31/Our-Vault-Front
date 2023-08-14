a4add36dad4bbea07fff2bff4de91b55a45dfe1e
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-14 09:23:08 -0500

    Creation of the project with the movie, serie, book and videogame components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { MovieComponent } from './movie/movie.component';
+import { SerieComponent } from './serie/serie.component';
+import { VideogameComponent } from './videogame/videogame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    MovieComponent,
+    SerieComponent,
+    VideogameComponent
   ],
   imports: [
     BrowserModule,
