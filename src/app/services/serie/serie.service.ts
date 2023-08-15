d99fd3ce07f60b8f3fcb578139d38350c51b32f5
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 10:29:20 -0500

    Creation of the GetSeries() into the SerieService class

--- src/app/services/serie/serie.service.ts
+++ src/app/services/serie/serie.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Serie } from 'src/app/models/serie.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SerieService {
+  private baseUrl = 'http://localhost:8090/our-vault/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSeries(): Observable<Serie[]> {
+    const url = `${this.baseUrl}/serie`;
+    return this.http.get<Serie[]>(url);
+  }
 }
