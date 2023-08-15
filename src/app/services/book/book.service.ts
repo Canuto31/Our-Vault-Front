f4ef1a2190499944acb5795912e8d31d91cb3517
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 10:19:09 -0500

    Creation of the getBook() service into que BookService class

--- src/app/services/book/book.service.ts
+++ src/app/services/book/book.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Book } from 'src/app/models/book.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+  private baseUrl = 'http://localhost:8090/our-vault/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBook(): Observable<Book[]> {
+    const url = `${this.baseUrl}/book`;
+    return this.http.get<Book[]>(url);
+  }
 }
