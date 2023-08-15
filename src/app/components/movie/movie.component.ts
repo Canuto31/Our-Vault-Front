a55e84dd64189308789a54198a57064526032d5f
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 11:59:28 -0500

    Creation of the component and html code for the Movie

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie.model';
+import { MovieService } from 'src/app/services/movie/movie.service';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
-export class MovieComponent {
+export class MovieComponent implements OnInit{
+  movies: Movie[] = [];
 
+  constructor(private movieService : MovieService) {};
+
+  ngOnInit(): void {
+      this.getMovies();
+  }
+
+  getMovies(): void {
+    this.movieService.getMovies().subscribe(movies => this.movies = movies);
+  }
 }
