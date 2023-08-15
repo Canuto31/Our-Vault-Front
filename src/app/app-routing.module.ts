5361b656bc7982453e00e95ae707aced3a503287
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-14 20:40:44 -0500

    Addition of the functionality about the authors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorComponent } from './components/author/author.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'authors', component: AuthorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/services/author/author.service.ts
+++ src/app/services/author/author.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Author } from 'src/app/models/author.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorService {
+  private baseUrl = 'http://localhost:8090/our-vault/api/others';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAuthors(): Observable<Author[]> {
+    const url = `${this.baseUrl}/author`;
+    return this.http.get<Author[]>(url);
+  }
 }
