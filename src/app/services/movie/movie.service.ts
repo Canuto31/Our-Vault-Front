e2ca58657665c08ff418b025ee2bd11e5927c1e1
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 10:26:42 -0500

    Creation of the GetMovies() into the MovieService class

--- src/app/services/movie/movie.service.ts
+++ src/app/services/movie/movie.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Movie } from 'src/app/models/movie.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private baseUrl = 'http://localhost:8090/our-vault/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMovies(): Observable<Movie[]> {
+    const url = `${this.baseUrl}/movie`;
+    return this.http.get<Movie[]>(url);;
+  }
 }
