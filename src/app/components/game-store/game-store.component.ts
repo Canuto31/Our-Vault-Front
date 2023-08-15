8fff1866c614a7073073e43b471fe1975b228b01
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 11:56:27 -0500

    Creation of the component and html code for the GameStore

--- src/app/components/game-store/game-store.component.ts
+++ src/app/components/game-store/game-store.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameStore } from 'src/app/models/gameStore.model';
+import { GameStoreService } from 'src/app/services/game-store/game-store.service';
 
 @Component({
   selector: 'app-game-store',
   templateUrl: './game-store.component.html',
   styleUrls: ['./game-store.component.css']
 })
-export class GameStoreComponent {
+export class GameStoreComponent implements OnInit {
+  gameStores: GameStore[] =[];
 
+  constructor(private gameStoreService: GameStoreService) {};
+
+  ngOnInit(): void {
+      this.getGameStores();
+  }
+
+  getGameStores(): void {
+    this.gameStoreService.getGameStores().subscribe(gameStores => this.gameStores = gameStores);
+  }
 }
