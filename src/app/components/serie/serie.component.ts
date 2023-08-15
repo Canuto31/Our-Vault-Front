80887911d57f393568f6e207f45779321d431e3a
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 12:02:10 -0500

    Creation of the component and html code for the Serie

--- src/app/components/serie/serie.component.ts
+++ src/app/components/serie/serie.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Serie } from 'src/app/models/serie.model';
+import { SerieService } from 'src/app/services/serie/serie.service';
 
 @Component({
   selector: 'app-serie',
   templateUrl: './serie.component.html',
   styleUrls: ['./serie.component.css']
 })
-export class SerieComponent {
+export class SerieComponent implements OnInit {
+  series: Serie[] = [];
 
+  constructor(private serieService : SerieService) {};
+
+  ngOnInit(): void {
+      this.getSeries();
+  }
+
+  getSeries(): void {
+    this.serieService.getSeries().subscribe(series => this.series = series);
+  }
 }
