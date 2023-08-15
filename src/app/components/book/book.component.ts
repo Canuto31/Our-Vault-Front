f02f77815adbfd083adc5035a541f4cc82a3291e
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 11:48:25 -0500

    Creation of the component and html code for the Book

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book.model';
+import { BookService } from 'src/app/services/book/book.service';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent {
+export class BookComponent implements OnInit {
+  books: Book[] = [];
 
+  constructor(private bookService: BookService) {};
+
+  ngOnInit(): void {
+      
+  }
+
+  getBooks(): void {
+    this.bookService.getBook().subscribe(books => this.books = books);
+  }
 }
