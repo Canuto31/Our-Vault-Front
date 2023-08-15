86b7b3b57445c42cf1ecf033bc6c3df86f9c8c9a
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 10:33:15 -0500

    Creation of the getStreamPages() into the StreamPageService class

--- src/app/services/stream-page/stream-page.service.ts
+++ src/app/services/stream-page/stream-page.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StreamPage } from 'src/app/models/streamPage.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StreamPageService {
+  private baseUrl = 'http://localhost:8090/our-vault/api/others';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getStreamPages(): Observable<StreamPage[]> {
+    const url = `${this.baseUrl}/stream-page`;
+    return this.http.get<StreamPage[]>(url);
+  }
 }
