f52facfbe0e366727c31fb069fa5a0baf0341e77
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 10:31:09 -0500

    Creation fo the getState() into the StateService class

--- src/app/services/state/state.service.ts
+++ src/app/services/state/state.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { State } from 'src/app/models/state.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateService {
+  private baseUrl = 'http://localhost:8090/our-vault/api/others';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getStates(): Observable<State[]> {
+    const url = `${this.baseUrl}/state`;
+    return this.http.get<State[]>(url);
+  }
 }
