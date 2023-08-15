b61d258a836ec285fbda46647a309f9a848a5d42
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 10:24:42 -0500

    Creation of the getGameStores() into the GameStoreService class

--- src/app/services/game-store/game-store.service.ts
+++ src/app/services/game-store/game-store.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GameStore } from 'src/app/models/gameStore.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameStoreService {
+  private baseUrl = 'http://localhost:8090/our-vault/api/others';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getGameStores(): Observable<GameStore[]> {
+    const url = `${this.baseUrl}/game-store`;
+    return this.http.get<GameStore[]>(url);
+  }
 }
