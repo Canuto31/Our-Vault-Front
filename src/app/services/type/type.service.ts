6dd67f20d5eae3c2cab0d99c6537a28f92c1190c
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 10:34:54 -0500

    Creation of the getTypes() into the TypeService class

--- src/app/services/type/type.service.ts
+++ src/app/services/type/type.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Type } from 'src/app/models/type.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TypeService {
+  private baseUrl = 'http://localhost:8090/our-vault/api/others';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTypes(): Observable<Type[]> {
+    const url = `${this.baseUrl}/type`;
+    return this.http.get<Type[]>(url);
+  }
 }
