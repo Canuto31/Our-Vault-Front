5736cee514626c246e1bb8c78946f1070754c30f
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 12:08:54 -0500

    Creation of the component and html code for the Type

--- src/app/components/type/type.component.ts
+++ src/app/components/type/type.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Type } from 'src/app/models/type.model';
+import { TypeService } from 'src/app/services/type/type.service';
 
 @Component({
   selector: 'app-type',
   templateUrl: './type.component.html',
   styleUrls: ['./type.component.css']
 })
-export class TypeComponent {
+export class TypeComponent implements OnInit {
+  types: Type[] = [];
+
+  constructor(private typeService: TypeService) {};
+
+  ngOnInit(): void {
+      this.getTypes();
+  }
+
+  getTypes(): void {
+    this.typeService.getTypes().subscribe(types => this.types = types);
+  }
 
 }
