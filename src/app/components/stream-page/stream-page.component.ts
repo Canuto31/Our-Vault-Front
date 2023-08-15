63bdd54fb46213b73b772ea7825cf98de1c6d7b7
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 12:06:32 -0500

    Creation of the component and html code for the StreamPage

--- src/app/components/stream-page/stream-page.component.ts
+++ src/app/components/stream-page/stream-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StreamPage } from 'src/app/models/streamPage.model';
+import { StreamPageService } from 'src/app/services/stream-page/stream-page.service';
 
 @Component({
   selector: 'app-stream-page',
   templateUrl: './stream-page.component.html',
   styleUrls: ['./stream-page.component.css']
 })
-export class StreamPageComponent {
+export class StreamPageComponent implements OnInit {
+  streamPages: StreamPage[] = [];
+
+  constructor(private streamPageService : StreamPageService) {};
+
+  ngOnInit(): void {
+      this.getStreamPages();
+  }
+
+  getStreamPages(): void {
+    this.streamPageService.getStreamPages().subscribe(streamPages => this.streamPages = streamPages);
+  }
 
 }
