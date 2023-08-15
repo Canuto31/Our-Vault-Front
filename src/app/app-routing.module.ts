f219921a427a57cbe7f6faa29bc337deffe401de
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 13:44:44 -0500

    Adding the routes for the correct functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthorComponent } from './components/author/author.component';
+import { BookComponent } from './components/book/book.component';
+import { CategoryComponent } from './components/category/category.component';
+import { GameStoreComponent } from './components/game-store/game-store.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { SerieComponent } from './components/serie/serie.component';
+import { StateComponent } from './components/state/state.component';
+import { StreamPageComponent } from './components/stream-page/stream-page.component';
+import { TypeComponent } from './components/type/type.component';
+import { VideogameComponent } from './components/videogame/videogame.component';
 
 const routes: Routes = [
-  { path: 'authors', component: AuthorComponent}
+  { path: 'authors', component: AuthorComponent},
+  { path: 'books', component: BookComponent},
+  { path: 'categories', component: CategoryComponent},
+  { path: 'gameStores', component: GameStoreComponent},
+  { path: 'movies', component: MovieComponent},
+  { path: 'series', component: SerieComponent},
+  { path: 'states', component: StateComponent},
+  { path: 'streamPages', component: StreamPageComponent},
+  { path: 'types', component: TypeComponent},
+  { path: 'videogames', component: VideogameComponent}
 ];
 
 @NgModule({
