2504135a1b72c4fe945fc354c4ce1c832b5ffc47
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 10:36:38 -0500

    Creation of the getVideogames() into the VideogameService class

--- src/app/services/videogame/videogame.service.ts
+++ src/app/services/videogame/videogame.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Videogame } from 'src/app/models/videogame.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideogameService {
+  private baseUrl = 'http://localhost:8090/our-vault/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getVideogames(): Observable<Videogame[]> {
+    const url = `${this.baseUrl}/videogame`;
+    return this.http.get<Videogame[]>(url);
+  }
 }
