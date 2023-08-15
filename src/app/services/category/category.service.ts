83fcc3c240ed867f4d6c4031cdf6e4619eada100
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 10:22:09 -0500

    Creation of the getCategories() service into the CategoryService class

--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/models/category.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  private baseUrl = 'http://localhost:8090/our-vault/api/others';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCategories(): Observable<Category[]> {
+    const url = `${this.baseUrl}/category`;
+    return this.http.get<Category[]>(url);
+  }
 }
