297df4cefcdfa0c390bfef15da334672f9ca4d32
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-08-15 12:04:22 -0500

    Creation of the component and html code for the State

--- src/app/components/state/state.component.ts
+++ src/app/components/state/state.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { State } from 'src/app/models/state.model';
+import { StateService } from 'src/app/services/state/state.service';
 
 @Component({
   selector: 'app-state',
   templateUrl: './state.component.html',
   styleUrls: ['./state.component.css']
 })
-export class StateComponent {
+export class StateComponent implements OnInit{
+  states: State[] = [];
 
+  constructor(private stateService : StateService) {};
+
+  ngOnInit(): void {
+      this.getState();
+  }
+
+  getState(): void {
+    this.stateService.getStates().subscribe(states => this.states = states);
+  }
 }
